dafff345f780bfbbde76bb3411a3c9cc888429a7
Rachit <merachit2898@gmail.com>
2024-02-10 18:37:18 +0530

    bugs fixed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import store from "./src/redux/store/store";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,32 +8,27 @@ import DashBoard from "./src/dashBoard";
 import Team from "./src/team";
 import Profile from "./src/profile";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const Root = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Dashboard"
-          screenOptions={{
-            animation: "slide",
-          }}
-        >
-          <Stack.Screen name="Dashboard" component={DashBoard} />
-          <Stack.Screen name="Team" component={Team} />
-          <Stack.Screen name="Profile" component={Profile} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Stack.Navigator initialRouteName="Dashboard">
+        <Stack.Screen name="Dashboard" component={DashBoard} />
+        <Stack.Screen name="Team" component={Team} />
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
     </SafeAreaView>
   );
 };
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Root />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <Root />
+      </Provider>
+    </NavigationContainer>
   );
 }
 
